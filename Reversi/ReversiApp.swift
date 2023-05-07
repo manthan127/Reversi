9c278637d4c86b0b89763f1e303ab7687e9e7201
manthan <gevariyamanthan8@gmail.com>
2023-05-07 18:10:39 +0530

    Initial Commit

---
+++ Reversi/ReversiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReversiApp.swift
+//  Reversi
+//
+//  Created by Home on 07/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ReversiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
